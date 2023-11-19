407202fa3e1d98370a878690f03ab993ce377d2c
chawkibekkouche <bekkouchechawki@yahoo.com>
2023-11-19 18:19:42 -0500

    film2

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import movieRoutes from './routes/movieRoutes.js';
-import { limiter } from './middleware/rateLimit.js';
 import { config } from 'dotenv';
+
 
 config();
 
 const app = express();
 
+
 app.use(express.json());
-app.use(limiter);
+
 app.use('/api/movies', movieRoutes);
 
 const PORT = process.env.PORT || 3000;
